2a638581e79010ae4961a0499618648ada89bd70
Daniel Morandini <danielmorandini@me.com>
2019-10-11 15:29:45 +0200

    Fix header

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright © 2019 booster authors
+// Copyright © 2019 KIM Keep In Mind GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
