a708c42a5af2afb146bd7267d51c296706d3bbc5
Daniel Morandini <danielmorandini@me.com>
2019-10-11 15:32:01 +0200

    Add package doc

--- pwrap/pwrap.go
+++ pwrap/pwrap.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package pwrap provides a process wrapper that is suitable to be executed
+// inside a tmux session, allowing to later retriving its stdout, stderr and
+// initial configuration.
 package pwrap
 
 import (
