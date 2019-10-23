d3eac0b3b5358d8fcf8ed81e07bf1d60cbaa783a
Daniel Morandini <danielmorandini@me.com>
2019-10-23 14:22:17 +0200

    Write updates on unix socket

--- cmd/mockcmd.go
+++ cmd/mockcmd.go
@@ -15,7 +15,13 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"sync"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -31,10 +37,21 @@ var MockcmdCmd = &cobra.Command{
 	Use:   "mockcmd",
 	Short: "A default mocked command which can be executed by pmux, but does not do anything useful.",
 	Run: func(cmd *cobra.Command, args []string) {
+		var w io.WriteCloser = os.Stdout
+		if sockPath != "" {
+			ctx := context.Background()
+			l, err := listen(ctx, sockPath)
+			if err != nil {
+				log.Printf("[ERROR] %v", err)
+				return
+			}
+			w = NewUpdatesWriter(ctx, l)
+		}
+
 		for i := 0; ; i++ {
-			fmt.Printf("waiting %d...", i)
+			fmt.Fprintf(w, "waiting %d...", i)
 			<-time.After(time.Millisecond * 1000)
-			fmt.Printf("done!\n")
+			fmt.Fprintf(w, "done!\n")
 		}
 	},
 }
@@ -42,4 +59,126 @@ var MockcmdCmd = &cobra.Command{
 func init() {
 	MockcmdCmd.Flags().StringVarP(&configPath, "config", "", "config.json", "Path to the configuration file.")
 	MockcmdCmd.Flags().StringVarP(&sockPath, "socket-path", "", "io.sock", "Path to the communication socket address.")
+}
+
+const defaultBufferCap = 4096
+
+// Listen starts a Unix Domain Socket listener on ``sockPath''.
+// Is is the caller's responsibility to close the listener when it's done.
+func listen(ctx context.Context, sockPath string) (net.Listener, error) {
+	if err := os.RemoveAll(sockPath); err != nil {
+		return nil, fmt.Errorf("unable to cleanup %v: %w", sockPath, err)
+	}
+
+	l, err := new(net.ListenConfig).Listen(ctx, "unix", sockPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to listen on %v: %w", sockPath, err)
+	}
+	return l, nil
+}
+
+func NewUpdatesWriter(ctx context.Context, l net.Listener) *UpdatesWriter {
+	uw := &UpdatesWriter{}
+	go uw.accept(ctx, l)
+	return uw
+}
+
+type UpdatesWriter struct {
+	last struct {
+		sync.Mutex
+		u *string
+	}
+	clients struct {
+		sync.Mutex
+		m map[string]chan string
+	}
+}
+
+func (w *UpdatesWriter) Write(p []byte) (int, error) {
+	if err := w.WriteUpdate(string(p)); err != nil {
+		return 0, err
+	}
+	return len(p), nil
+}
+
+func (w *UpdatesWriter) Close() error {
+	return nil
+}
+
+func (w *UpdatesWriter) WriteUpdate(u string) error {
+	w.last.Lock()
+	w.last.u = &u
+	w.last.Unlock()
+
+	w.clients.Lock()
+	defer w.clients.Unlock()
+	for _, v := range w.clients.m {
+		v <- u
+	}
+	return nil
+}
+
+type rx struct {
+	close func()
+	c     <-chan string
+}
+
+func (w *UpdatesWriter) getTx() *rx {
+	tx := make(chan string, 1)
+
+	w.last.Lock()
+	// generate a timestamp key inside the lock, so we're ensured to receive a unique one.
+	key := fmt.Sprintf("%d", time.Now().UnixNano())
+	if w.last.u != nil {
+		tx <- *w.last.u
+	}
+	w.last.Unlock()
+
+	w.clients.Lock()
+	if w.clients.m == nil {
+		w.clients.m = make(map[string]chan string)
+	}
+	w.clients.m[key] = tx
+	w.clients.Unlock()
+
+	return &rx{
+		c: tx,
+		close: func() {
+			close(tx)
+			w.clients.Lock()
+			delete(w.clients.m, key)
+			w.clients.Unlock()
+		},
+	}
+}
+
+func (w *UpdatesWriter) accept(ctx context.Context, l net.Listener) {
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Printf("[ERROR] uds listener: %v", err)
+			return
+		}
+
+		rx := w.getTx()
+		go serveUpdates(ctx, conn, rx)
+	}
+}
+
+func serveUpdates(ctx context.Context, conn net.Conn, rx *rx) {
+	defer conn.Close()
+	defer rx.close()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Printf("[INFO] closing connection to %v: %v", conn.RemoteAddr().String(), ctx.Err())
+		case u := <-rx.c:
+			// Note: If the connection is closed, we will not be able to detect it
+			// util the next time that we try to write something into it.
+			if _, err := conn.Write([]byte(u)); err != nil {
+				log.Printf("[ERROR] unable to write update to connection %v: %v", conn.RemoteAddr().String(), err)
+				return
+			}
+		}
+	}
 }
